42fafecd0846a4013655f4063e3ffa90b28002af
Adrian Ruiz <adriancabezon@gmail.com>
2025-06-03 19:40:29 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
 
               <div className="flex items-center gap-3 text-gray-600">
                 <Clock className="w-5 h-5" />
-                <p>Lunes a Sábado: 9:30 - 20:00</p>
+                <p>Lunes a Sábado: 8:30 a 12 hs - 17 a 20:00 hs</p>
               </div>
 
               <div className="flex items-center gap-3 text-gray-600">
